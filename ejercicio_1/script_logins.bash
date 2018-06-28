fc1c956440b2cce66aa6ab75e6582e25051cb04c
Valentina Tironi <valentina@valentina.anima.edu.uy>
2018-06-28 12:54:21 -0300

    casi listo ejercicio 1, falta que imprima todos los logs cuando el usuario existe y no tiene logs

--- ejercicio_1/script_logins.bash
+++ ejercicio_1/script_logins.bash
@@ -1,11 +1,26 @@
 echo "Ingrese el nombre del usuario: "
 read usuario
 
-last -w -F | awk -v usuario=$usuario '{ 
-	if (usuario == $1) {
-			print "El usuario " usuario " ingresó el " $5 " " $4 " " $7 " a las " $6 " y salió el " $11 " " $10 " a las " $12
-	} else if (usuario == ""){
-			print "El usuario " $1 " ingresó el " $5 " " $4 " " $7 " a las " $6 " y salió el " $11 " " $10 " a las " $12
-	}
-}'
+user_exist=$(cat /etc/passwd | grep "$usuario")
 
+if [ -z "$usuario" ] || [ -z "$user_exist" ]
+then 
+	echo "EL USUARIO INGRESADO NO TIENE LOGS O NO EXISTE. 
+       ******SE MUESTRAN LOS LOGS Y LOGINS DE TODOS LOS USUARIOS******"
+else 
+	echo "******LOGS Y LOGINS DEL USUARIO $usuario******"
+fi
+
+files=$(find /var/log/ | grep wtmp)
+
+for file in $files 
+do
+
+	last -f $file -w | awk -v usuario="$usuario" -v user_exist="$user_exist" '{ 
+		if (usuario == "" || user_exist == ""){
+			print $0
+		} else if (usuario == $1) {
+			print $0
+		}
+	}'
+done
