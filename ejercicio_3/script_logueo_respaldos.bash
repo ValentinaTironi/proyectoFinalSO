46bab27593101c301862c1f22f4229a8b38e466f
Valentina Tironi <valentina@valentina.anima.edu.uy>
2018-07-19 12:15:59 -0300

    terminado

--- ejercicio_3/script_logueo_respaldos.bash
+++ ejercicio_3/script_logueo_respaldos.bash
@@ -1,10 +1,12 @@
+
 respaldos_path="/media/sf_shared_host_folder/respaldos"
 
 mkdir -p $respaldos_path
 
-current_day=$(date +"%Y_%m_%d")
+current_date=$(date +"%Y_%m_%d")
 
 directories="/etc /home /var/lib"
-echo $current_day
 
-tar -czvf $respaldos_path/respaldo_$current_day.tar.gz $directories
+tar -czvf $respaldos_path/respaldo_$current_date.tar.gz $directories
+
+echo "El respaldo se hizo correctamente en la fecha $current_date" >> /var/log/mis_respaldos.txt 
