a922aa5966508b28f77d76878b3af255ca30a945
root <root@valentina.anima.edu.uy>
2018-07-20 15:16:11 -0300

    ejercicio 3 terminado

--- ejercicio_3/script_logueo_respaldos.bash
+++ ejercicio_3/script_logueo_respaldos.bash
@@ -5,8 +5,17 @@ mkdir -p $respaldos_path
 
 current_date=$(date +"%Y_%m_%d")
 
-directories="/etc /home /var/lib"
+directories="/etc /hme /var/lib"
 
 tar -czvf $respaldos_path/respaldo_$current_date.tar.gz $directories
 
-echo "El respaldo se hizo correctamente en la fecha $current_date" >> /var/log/mis_respaldos.txt 
+stat=$(echo $?)
+
+if [ "$stat" = "0" ]
+then
+	echo "success - El respaldo se hizo correctamente - $current_date" >> /var/log/mis_respaldos.txt
+else
+	echo "El respaldo no fue completado con exito, por favor vuelva a intentarlo"
+	echo "error - El respaldo no fue completado con éxito - $current_date" >> /var/log/mis_respaldos.txt
+	mv $respaldos_path/respaldo_$current_date.tar.gz $respaldos_path/error_respaldo_$current_day.tar.gz
+fi	
