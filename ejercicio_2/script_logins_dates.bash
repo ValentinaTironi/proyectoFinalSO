cce8a6c8a031ce20bf0b6ef5d190fe174de6fc4f
Alumno Anima <alumno@sistemasoperativos.anima.edu.uy>
2018-07-24 09:40:01 -0400

    ejercicio 2 termiado

--- ejercicio_2/script_logins_dates.bash
+++ ejercicio_2/script_logins_dates.bash
@@ -6,22 +6,40 @@ echo "Inserte fecha de finalizacion (YYYY-MM-DD):"
 
 read fecha_hasta
 
+files=$(find /var/log/ | grep  wtmp)
 if [ -z "$fecha_hasta" ] && [ ! -z "$fecha_desde" ]
 then 
 	echo "** No se ingreso fecha de finalizacion, se van a mostrar todos los registros desde la fecha $fecha_desde hasta el dia de hoy **"	
-	last -s "$fecha_desde"
+	for file in $files
+	do
+		last -f $file -s "$fecha_desde"
+	done
 elif [ -z "$fecha_desde" ] && [ ! -z "$fecha_hasta" ]
 then
-	echo "** No se imgreso fecha de comienzo, se van a mostrar todos los registros hasta la fecha $fecha_hasta **"
+	echo "** No se ingreso fecha de comienzo, se van a mostrar todos los registros hasta la fecha $fecha_hasta **"
 	last -t "$fecha_hasta"
-
+	for file in $files
+	do
+		last -f $file -s "$fecha_hasta"
+	done
 elif [ -n "$fecha_hasta" ] && [ -n "$fecha_desde" ]
 then
-	if [ "$fecha_desde" < "$fecha_hasta" ]
-	then
-		last -s "$fecha_desde" -t "$fecha_hasta"
+	if [[ "$fecha_desde" < "$fecha_hasta" ]]
+	then	
+		last_command="$(for file in $files
+		do
+			last -f $file -s "$fecha_desde" -t "$fecha_hasta"
+		done)"
+		lol=$(echo "${last_command}" | grep 'reboot')
+		if [ -z "$lol" ]
+		then
+			echo "Todo mal"
+		else
+			echo "$last_command"
+			echo "todo bien"
+		fi	
+	else
+		echo "No es posible mostrar ningun registro dado a que ha ingresado incorrecamente las fechas requeridas"
 	fi
-
 fi
-
 
