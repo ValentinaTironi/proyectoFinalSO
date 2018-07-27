7245667983efd0b076540096aafae6b4eadc3cfd
root <root@sistemasoperativos.anima.edu.uy>
2018-07-26 20:23:10 -0400

    fix

--- ejercicio_3/script_logueo_respaldos.bash
+++ ejercicio_3/script_logueo_respaldos.bash
@@ -4,18 +4,19 @@ respaldos_path="/media/sf_shared_host_folder/respaldos"
 mkdir -p $respaldos_path
 
 current_date=$(date +"%Y_%m_%d")
+current_time=$(date +"%H:%M:%S")
 
-directories="/etc /hme /var/lib"
+directories="/etc /home /var/lib"
 
-tar -czvf $respaldos_path/respaldo_$current_date.tar.gz $directories
+tar -czf $respaldos_path/respaldo_$current_date.tar.gz $directories
 
 stat=$(echo $?)
 
 if [ "$stat" = "0" ]
 then
-	echo "success - El respaldo se hizo correctamente - $current_date" >> /var/log/mis_respaldos.txt
+	echo "success - El respaldo se hizo correctamente - $current_date $current_time" >> /var/log/mis_respaldos.txt
 else
 	echo "El respaldo no fue completado con exito, por favor vuelva a intentarlo"
-	echo "error - El respaldo no fue completado con éxito - $current_date" >> /var/log/mis_respaldos.txt
+	echo "error - El respaldo no fue completado con éxito - $current_date $current_time" >> /var/log/mis_respaldos.txt
 	mv $respaldos_path/respaldo_$current_date.tar.gz $respaldos_path/error_respaldo_$current_day.tar.gz
 fi	
