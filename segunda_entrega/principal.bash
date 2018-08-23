de014dfb51b21e17f692454a16c62bb23089dabd
Valentina Tironi <valentina@valentina.anima.edu.uy>
2018-08-23 12:40:11 -0300

    organizar

---
+++ segunda_entrega/principal.bash
@@ -0,0 +1,7 @@
+echo Selecciona una de las siguientes opciones:
+
+echo "
+1. Configurar archivo para respaldo
+2. Configurar directorio para respaldo
+3. Configurar CRON
+4. Listar crontab del usuario"
